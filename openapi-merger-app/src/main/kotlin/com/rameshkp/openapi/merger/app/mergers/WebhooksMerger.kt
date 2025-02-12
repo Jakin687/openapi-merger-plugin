c776e931705c4fbb9ca6192144ce88486fe54c95
Jakin687 <100352062+Jakin687@users.noreply.github.com>
2025-02-12 10:59:52 +0100

    Update WebhooksMerger.kt

--- openapi-merger-app/src/main/kotlin/com/rameshkp/openapi/merger/app/mergers/WebhooksMerger.kt
+++ openapi-merger-app/src/main/kotlin/com/rameshkp/openapi/merger/app/mergers/WebhooksMerger.kt
@@ -1,10 +1,10 @@
 package com.rameshkp.openapi.merger.app.mergers
 
-import io.swagger.v3.oas.models.Paths
+import io.swagger.v3.oas.models.OpenAPI
 
 class WebhooksMerger: PathsMerger() {
-    override fun get(): Paths {
-        val webhook = Paths()
+    override fun get(): OpenApi {
+        val webhook = OpenApi()
         map.forEach { (key, value) ->  webhook.addWebhooks(key, value)}
         return webhook
     }

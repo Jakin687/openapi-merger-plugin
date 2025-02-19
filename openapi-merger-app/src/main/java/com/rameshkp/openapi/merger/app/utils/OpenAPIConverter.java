7ee5d3a41e39c71ac5e1a3e608b7043f50f4e0de
Jakin687 <100352062+Jakin687@users.noreply.github.com>
2025-02-19 09:36:15 +0100

    Update OpenAPIConverter.java
    The converter provided by Swagger cannot distinguish between api with versions below or above 3.1.0. Therefore, this needs to be checked independently by the OpenAPIConverter.

--- openapi-merger-app/src/main/java/com/rameshkp/openapi/merger/app/utils/OpenAPIConverter.java
+++ openapi-merger-app/src/main/java/com/rameshkp/openapi/merger/app/utils/OpenAPIConverter.java
@@ -2,7 +2,9 @@ package com.rameshkp.openapi.merger.app.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.core.util.Json;
+import io.swagger.v3.core.util.Json31;
 import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.core.util.Yaml31;
 import io.swagger.v3.oas.models.OpenAPI;
 
 /**
@@ -10,6 +12,8 @@ import io.swagger.v3.oas.models.OpenAPI;
  Implementing the below methods in kotlin is causing a weired issue where json and yaml are not serialized properly
  */
 public class OpenAPIConverter {
+    private static final String V31 = "3.1";
+
     /**
      * Provided an open api object this method converts it to a json string
      * @param openAPI object to convert
@@ -17,6 +21,7 @@ public class OpenAPIConverter {
      * @throws JsonProcessingException when json parsing fails
      */
     public static String toJson(OpenAPI openAPI) throws JsonProcessingException {
+        if (is31(openAPI)) return Json31.pretty().writeValueAsString(openAPI);
         return Json.pretty().writeValueAsString(openAPI);
     }
 
@@ -27,6 +32,11 @@ public class OpenAPIConverter {
      * @throws JsonProcessingException when yaml parsing fails
      */
     public static String toYaml(OpenAPI openAPI) throws JsonProcessingException {
+        if (is31(openAPI)) return Yaml31.pretty().writeValueAsString(openAPI);
         return Yaml.pretty().writeValueAsString(openAPI);
+    }
+
+    private static boolean is31(OpenAPI openAPI) {
+        return openAPI.getOpenapi().startsWith(V31);
     }
 }
